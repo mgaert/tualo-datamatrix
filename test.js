62fb0bfee9fb304ea8a61934027bcf28289b882b
Mobile <mobile@Mobiles-MacBook-Pro.local>
2015-12-01 19:40:15 +0100

    + add function toString() and getMatrix() to the Matrix class. + removed debug messages + changed return value of getDigit() of class Datamatrix. Now return an Object of class Matrix.

--- test.js
+++ test.js
@@ -2,8 +2,8 @@
 var Datamatrix = require('./lib/datamatrix').Datamatrix;
 var dm = new Datamatrix();
 var ascii = dm.getDigit('http://tualo.de',{rectangular:false,addFNC1:false});
-console.log(ascii);
+console.log(ascii.toString());
 var ascii2 = dm.getDigit('http://tualo.de',{rectangular:false,addFNC1:true});
-console.log(ascii2);
+console.log(ascii2.toString());
 var ascii3 = dm.getDigit('http://tualo.de',{rectangular:false,addFNC1:false,length:144 /* 44x44 Matrix */});
-console.log(ascii3);
+console.log(ascii3.toString());
